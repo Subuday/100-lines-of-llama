baeabf86dc7ba439dcc8aabd6f9ee8fec6671b11
Maksym Sutkovenko <msutkovenko@readdle.com>
2024-01-08 00:56:50 +0200

    Implement inference

--- inference.py
+++ inference.py
@@ -1,9 +1,12 @@
 import json
 import time
 from pathlib import Path
+from typing import List
 
 import torch
 from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+
 from model import ModelArgs, Transformer
 
 
@@ -58,21 +61,94 @@ class LLaMa:
 
         return LLaMa(model, tokenizer, model_args)
 
+    def text_completion(self, prompts: List[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len=None):
+        if max_gen_len is None:
+            max_gen_len = self.args.max_seq_len - 1
+        prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        batch_size = len(prompt_tokens)
+        assert batch_size <= self.args.max_batch_size, f"Batch size {batch_size} is too large"
+        max_prompt_lem = max(len(prompt) for prompt in prompt_tokens)
+        assert max_prompt_lem <= self.args.max_seq_len, f"Prompt length {max_prompt_lem} is too large"
+        total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_lem)
+
+        pad_id = self.tokenizer.pad_id()
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
+        for k, t in enumerate(prompt_tokens):
+            tokens[k, :len(t)] = torch.tensor(t, dtype=torch.long, device=device)
+
+        eos_reached = torch.tensor([False] * batch_size).to(device)
+        prompt_tokens_mask = tokens != pad_id
+        for cur_pos in tqdm(range(1, total_len), desc="Generating tokens"):
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos - 1:cur_pos], cur_pos)
+            if temperature > 0:
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                next_token = torch.argmax(logits[:, -1], dim=-1)
+
+            next_token = next_token.reshape(-1)
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id())
+            if all(eos_reached):
+                break
+
+        out_tokens = []
+        out_text = []
+        for prompt_index, current_prompt_tokens in enumerate(tokens.tolist()):
+            if self.tokenizer.eos_id() in current_prompt_tokens:
+                eos_idx = current_prompt_tokens.index(self.tokenizer.eos_id())
+                current_prompt_tokens = current_prompt_tokens[:eos_idx]
+            out_tokens.append(current_prompt_tokens)
+            out_text.append(self.tokenizer.decode(current_prompt_tokens))
+
+        return out_tokens, out_text
+
+    def _sample_top_p(self, probs, p):
+        probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+        probs_sum = torch.cumsum(probs_sort, dim=-1)
+        # TODO: Investigate this
+        mask = probs_sum - probs_sort > p
+        probs_sort[mask] = 0
+        probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+        next_token = torch.multinomial(probs_sort, num_samples=1)
+        next_token = torch.gather(probs_idx, -1, next_token)
+        return next_token
+
 
 if __name__ == "__main__":
     torch.manual_seed(0)
 
     device = "cpu"
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
+
+    prompts = [
+        "Simply put, the theory of relativity states that ",
+        "If Google was an Italian company founded in Milan, it would",
+
+        """Translate English to French:
+
+        sea otter => loutre de mer
+        peppermint => menthe poivrée
+        plush girafe => girafe peluche
+        cheese =>""",
+
+        "Apple was founded by"
+    ]
 
     model = LLaMa.build(
         checkpoints_dir="llama-2-7b",
         tokenizer_path="tokenizer.model",
         load_model=True,
         max_seq_len=1024,
-        max_batch_size=3,
+        max_batch_size=len(prompts),
         device=device
     )
+
+    out_tokens, out_text = model.text_completion(prompts, max_gen_len=64)
+    assert len(out_tokens) == len(prompts)
+    for i in range(len(out_text)):
+        print(f"{out_text[i]}")
+        print('-' * 50)
+
+
