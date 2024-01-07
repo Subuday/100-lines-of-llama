6cf78f6b93a87fa9d087816335f3b0ff71f66530
Maksym Sutkovenko <msutkovenko@readdle.com>
2024-01-07 14:51:16 +0200

    Implement RSMNorm

--- model.py
+++ model.py
@@ -39,6 +39,21 @@ def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device
     x_out = x_out.reshape(*x.shape)
     return x_out.type_as(x).to(device)
 
+
+class RSMNorm(nn.Module):
+
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x: torch.Tensor):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x: torch.Tensor):
+        return self.weight * self._norm(x.float()).type_as(x)
+
+
 class Transformer(nn.Module):
 
     def __init__(self, args: ModelArgs):
